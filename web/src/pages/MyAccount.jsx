a612abd786695dd8b85761d7f4379dc20516f5a7
Mohamed Amine <harchimedamine2004l@gmail.com>
2024-11-07 00:32:49 +0100

    TESTING

--- web/src/pages/MyAccount.jsx
+++ web/src/pages/MyAccount.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
 
 function MyAccount() {
   return (
-    <div>MyAccount</div>
-  )
+    <div className="w-full h-screen bg-[#ffffe3]">
+      <div className="w-4/5 h-screen bg-white p-10 m-auto mt-[20px] pr-[200px]">
+        <div className=" bg-blue-500 h-[250px] w-full mt-0 p-0 rounded-t-[32px]">
+          <img src="" alt="" />
+          <div>
+            <img
+              src="default-img.jpg"
+              alt="Profil Pic"
+              class="w-32 h-32 rounded-full object-cover border-2 mt-[220px] ml-[20px]"
+            />
+          </div>
+        </div>
+        <p className="font-serif text-[40px] ml-[190px] mt-[20px]">Mohamed Amine Harchi</p>
+      </div>
+    </div>
+  );
 }
 
-export default MyAccount
+export default MyAccount;
