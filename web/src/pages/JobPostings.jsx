89dda93a3556fe92ec9f8cec3154b4af0dfd6beb
DazaiCR <chaimaeattouch000@gmail.com>
2024-11-11 11:42:45 +0100

    Add style to sidebar

--- web/src/pages/JobPostings.jsx
+++ web/src/pages/JobPostings.jsx
@@ -10,22 +10,25 @@ function JobPostings() {
 
   return (
     <div className='w-screen h-screen'>
-      <div className='grid grid-cols-4'>
+      <div className='flex flex-row gap-4 bg-[#f4f4f5] nunito-font'>
         {/* Job Cards Section */}
-        <div className='col-span-1 p-4'>
+        <div className='bg-white my-4 ml-4 px-4 rounded shadow-xl h-screen text-[#1A5632] font-bold text-lg  basis-1/4 max-w-1/4'>
+
+          <div className='border-b-2 m-3 mt-6 pb-2 pl-0 border-[#1A5632]'>Jobs</div>
+
           {jobs.map((job) => (
             <div 
               key={job.id} 
-              className='p-4 mb-2 cursor-pointer rounded shadow border'
+              className='m-3 my-5 pr-3 cursor-pointer text-wrap hover:text-[#2E7A47] max-w-full hover:bg-gray-100 hover:pl-7 hover:rounded hover:shadow-sm hover:duration-300 hover:underline hover:py-3'
               onClick={() => handleCardClick(job)}
             >
-              {job.title}
+              {job.title} 
             </div>
           ))}
         </div>
 
         {/* Job Details Section */}
-        <div className='col-span-3 p-4'>
+        <div className='bg-white flex-1 my-4 mr-4 rounded shadow-md h-screen'>
           {selectedJob ? (
             <div>
               <div>
